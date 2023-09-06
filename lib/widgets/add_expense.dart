054bf38519c826c4310ba352a6d67e9d01cb3c23
mylistrikapp@gmail.com <bimataruna21@gmail.com>
2023-09-06 11:02:48 +0700

    add more input to add expense modal

--- lib/widgets/add_expense.dart
+++ lib/widgets/add_expense.dart
@@ -10,15 +10,42 @@ class AddExpense extends StatefulWidget {
 }
 
 class _AddExpenseState extends State<AddExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(children: [
         TextField(
+          controller: _titleController,
           maxLength: 50,
-          decoration: InputDecoration(label: Text("title")),
+          decoration: const InputDecoration(label: Text("title")),
         ),
+        TextField(
+          controller: _amountController,
+          keyboardType: TextInputType.number,
+          maxLength: 20,
+          decoration: const InputDecoration(label: Text("Amount")),
+        ),
+        Row(
+          children: [
+            TextButton(onPressed: () {}, child: const Text("Cancel")),
+            ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: const Text("Save Expense"))
+          ],
+        )
       ]),
     );
   }

953557eeb169d29a93bbda55fa25943e659eb752
mylistrikapp@gmail.com <bimataruna21@gmail.com>
2023-09-06 08:58:51 +0700

    adding icon to expense category

--- lib/models/expense_model.dart
+++ lib/models/expense_model.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
 enum Category { food, leisure, travel, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work
+};
 
 class Expense {
   Expense(
@@ -17,4 +25,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  // String get formattedDate {
+  //   return
+  // }
 }

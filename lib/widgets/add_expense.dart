19c1ffb52da72764def09fb29416b938e6a9161e
mylistrikapp@gmail.com <bimataruna21@gmail.com>
2023-09-07 11:29:44 +0700

    add datepicker

--- lib/widgets/add_expense.dart
+++ lib/widgets/add_expense.dart
@@ -1,3 +1,4 @@
+import 'package:expense_project/models/expense_model.dart';
 import 'package:flutter/material.dart';
 
 class AddExpense extends StatefulWidget {
@@ -12,6 +13,22 @@ class AddExpense extends StatefulWidget {
 class _AddExpenseState extends State<AddExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate = DateTime.now();
+
+  void _openDatePicker() async {
+    final dateNow = DateTime.now();
+    final firstDate = DateTime(dateNow.year - 1, dateNow.month, dateNow.day);
+
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: dateNow,
+        firstDate: firstDate,
+        lastDate: dateNow);
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -23,22 +40,47 @@ class _AddExpenseState extends State<AddExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(20),
       child: Column(children: [
         TextField(
           controller: _titleController,
           maxLength: 50,
           decoration: const InputDecoration(label: Text("title")),
         ),
-        TextField(
-          controller: _amountController,
-          keyboardType: TextInputType.number,
-          maxLength: 20,
-          decoration: const InputDecoration(label: Text("Amount")),
-        ),
         Row(
           children: [
-            TextButton(onPressed: () {}, child: const Text("Cancel")),
+            Expanded(
+              child: TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    prefixText: 'Rp', label: Text("Amount")),
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(formatter.format(_selectedDate!)),
+                IconButton(
+                    onPressed: _openDatePicker,
+                    icon: const Icon(Icons.calendar_month))
+              ],
+            ))
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel")),
             ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);

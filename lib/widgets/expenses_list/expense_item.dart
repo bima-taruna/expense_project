7ae6753c6bfce14ab6757454eec2b2ca75b86171
mylistrikapp@gmail.com <bimataruna21@gmail.com>
2023-09-09 16:10:20 +0700

    change the title style in expense item

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -12,12 +12,12 @@ class ExpenseItem extends StatelessWidget {
     return Card(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: Column(children: [
-          Text(expense.title),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(expense.title, style: Theme.of(context).textTheme.titleLarge),
           const SizedBox(height: 4),
           Row(
             children: [
-              Text("\$ ${expense.amount.toStringAsFixed(2)}"),
+              Text("Rp ${expense.amount.toStringAsFixed(2)}"),
               const Spacer(),
               Row(
                 children: [

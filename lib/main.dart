352379ea59a8b4b21e52b79c2cc9296603ac0c52
mylistrikapp@gmail.com <bimataruna21@gmail.com>
2023-09-09 10:41:33 +0700

    theming the app bar and background

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:expense_project/widgets/expense.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 167, 74, 255));
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+            centerTitle: true,
+          )),
       home: const Expenses(),
     ),
   );

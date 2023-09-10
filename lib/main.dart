fe1d7d99a5121da008b089939302a3b157a27976
mylistrikapp@gmail.com <bimataruna21@gmail.com>
2023-09-11 06:10:00 +0700

    add dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,25 @@ import 'package:flutter/material.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 167, 74, 255));
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 5, 99, 125));
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        )),
+      ),
       theme: ThemeData().copyWith(
           useMaterial3: true,
           colorScheme: kColorScheme,
@@ -26,6 +42,7 @@ void main() {
                   fontWeight: FontWeight.bold,
                   color: kColorScheme.onSecondaryContainer,
                   fontSize: 16))),
+      themeMode: ThemeMode.dark,
       home: const Expenses(),
     ),
   );
